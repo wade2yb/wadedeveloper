23f3431fc86108b75b353369d8e26a268b6afe04
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-08 04:50:25 +0700

    Update invite.js

--- commands/help/invite.js
+++ commands/help/invite.js
@@ -10,7 +10,7 @@ module.exports = {
     .setTitle(`HERE INVITE LINK OF BOT`)
     .setDescription(`743830477818953768>[CLICK HERE](https://discord.com/oauth2/authorize?client_id=818215899987181650&scope=bot&permissions=1547173503) OR [support server ](https://discord.gg/Enyvu8dv38)`)
     .setColor("RANDOM")
-    .setFooter(`BOT MADE BY Unio77#8580`)
+    .setFooter(`BOT MADE BY Kyow.#8580`)
     .setTimestamp(message.timestamp = Date.now())
     
     message.channel .send(embed)
