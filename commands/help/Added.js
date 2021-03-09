d2bf99b12b9559a86df15ae06194e2d4a1e037d0
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-09 12:46:16 +0700

    Update Added.js

--- commands/help/Added.js
+++ commands/help/Added.js
@@ -6,10 +6,10 @@ module.exports = {
     category: "modration",
     run: async (client, message, args) => {
         if (!message.member.hasPermission("MANAGE_EMOJIS")) {
-return message.channel.send(`:x: | **You Don't Have Permission To Use This Command**`)
+return message.channel.send(`:x: | **Anda Tidak Memiliki Izin Untuk Menggunakan Perintah Ini**`)
 }
         const emojis = args.join(" ").match(/<?(a)?:?(\w{2,32}):(\d{17,19})>?/gi)
-        if (!emojis) return message.channel.send(`:x: | **Provde The emojis to add**`);
+        if (!emojis) return message.channel.send(`:x: | **Sediakan emoji untuk ditambahkan**`);
         emojis.forEach(emote => {
         let emoji = Discord.Util.parseEmoji(emote);
         if (emoji.id) {
@@ -20,7 +20,7 @@ return message.channel.send(`:x: | **You Don't Have Permission To Use This Comma
                 `${Link}`,
                 `${`${emoji.name}`}`
             ).then(em => message.channel.send(em.toString() + " added!")).catch(error => {
-              message.channel.send(":x: | an Error occured")
+              message.channel.send(":x: | kesalahan terjad")
                 console.log(error)
 })
           
