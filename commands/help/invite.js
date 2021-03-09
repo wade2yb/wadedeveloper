8888d53e77cd93d82ee1b4716d6a2d08d4a0da15
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-09 12:48:20 +0700

    Update invite.js

--- commands/help/invite.js
+++ commands/help/invite.js
@@ -3,14 +3,14 @@ const discord = require("discord.js");
 module.exports = {
   name: "invite",
   category: "info",
-  description: "INVITE Automodv12 beta BOT",
+  description: "INVITE WADE CHAN",
   run: async (client, message, args) => {
     
     let embed = new discord.MessageEmbed()
     .setTitle(`HERE INVITE LINK OF BOT`)
     .setDescription(`[CLICK HERE](https://discord.com/oauth2/authorize?client_id=818215899987181650&scope=bot&permissions=1547173503) OR [support server ](https://discord.gg/Enyvu8dv38)`)
     .setColor("RANDOM")
-    .setFooter(`BOT MADE BY Kyow.#8580`)
+    .setFooter(`BOT Made Kyow.#8580`)
     .setTimestamp(message.timestamp = Date.now())
     
     message.channel .send(embed)
