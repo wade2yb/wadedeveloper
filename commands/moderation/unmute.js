e0b372fec7ca8cae1841f0b70e1def9f73154862
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-09 12:57:48 +0700

    Update unmute.js

--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -6,31 +6,31 @@ module.exports = {
   run: async (client, message, args) => {
     if (!message.member.hasPermission("MANAGE_ROLES")) {
       return message.channel.send(
-        "Sorry but you do not have permission to unmute anyone"
+        "Maaf, tetapi Anda tidak memiliki izin untuk menyuarakan siapa pun"
       );
     }
 
     if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
-      return message.channel.send("I do not have permission to manage roles.");
+      return message.channel.send("Saya tidak memiliki izin untuk Manage Roles.");
     }
 
     const user = message.mentions.members.first();
 
     if (!user) {
-      return message.channel.send("Please mention the member to who you want to unmute");
+      return message.channel.send("Harap sebutkan anggota yang ingin Anda tampilkan lagi");
     }
 
     let muterole = message.guild.roles.cache.find(x => x.name === "Muted");
 
     if (user.roles.cache.has(muterole)) {
-      return message.channel.send("Given User do not have mute role so what i am suppose to take");
+      return message.channel.send("Mengingat Pengguna tidak memiliki peran bisu jadi apa yang harus saya ambil");
     }
 
     user.roles.remove(muterole)
 
     await message.channel.send(`**${message.mentions.users.first().username}** is now unmuted`);
 
-    user.send(`You are now unmuted from **${message.guild.name}**`);
+    user.send(`Anda sekarang di unmute dari **${message.guild.name}**`);
     
     message.delete()
   }
