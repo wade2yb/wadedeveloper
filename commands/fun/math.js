4f08970eb215846cfa30c17c8623aae1e1840653
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-09 12:39:39 +0700

    Update math.js

--- commands/fun/math.js
+++ commands/fun/math.js
@@ -7,7 +7,7 @@ module.exports = {
   category: "fun",
   run: async (client, message, args) => {
     try {
-      if (!args[0]) return message.channel.send("Please Give Me Equation!");
+      if (!args[0]) return message.channel.send("Tolong Beri Aku Persamaan!");
 
       const embed = new MessageEmbed()
         .setColor(`${Color}`)
@@ -17,7 +17,7 @@ module.exports = {
 
       message.channel.send(embed);
     } catch (error) {
-      message.channel.send(`Please Give Me Valid Equation | Try Again Later!`).then(() => console.log(error));
+      message.channel.send(`Tolong Beri Saya Persamaan yang Valid | Coba lagi nanti!`).then(() => console.log(error));
     }
   }
 };
