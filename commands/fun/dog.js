501b9e9b51fec0e3d4f5d3f52629812874d64c72
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-09 12:33:21 +0700

    Update dog.js

--- commands/fun/dog.js
+++ commands/fun/dog.js
@@ -6,17 +6,17 @@ const Discord = require('discord.js')
 module.exports = {
   name: "dog",
   category: "fun",
-description: "Sends a random dog image",
+description: "Mengirim gambar anjing secara acak",
 usage: "[command]",
 run: async (client, message, args) => {
 //command
 superagent.get('https://nekos.life/api/v2/img/woof')
     .end((err, response) => {
   const lewdembed = new Discord.MessageEmbed()
-  .setTitle("<a:dog1:730389376210829344>")
+  .setTitle("DOG")
   .setImage(response.body.url)
   .setColor(`#000000`)
-  .setFooter(`🤣WHAT A DOG🤣`)
+  .setFooter(`DOGE`)
   .setURL(response.body.url);
 message.channel.send(lewdembed);
 })
