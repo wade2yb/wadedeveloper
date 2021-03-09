96e952b5e71a8960fd883b1ad9ce75f82bf754de
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-09 12:41:19 +0700

    Update pat.js

--- commands/fun/pat.js
+++ commands/fun/pat.js
@@ -5,7 +5,7 @@ const random = new Random();
 module.exports = {
   name: "pat",
   category: "fun",
-  description: "Pat someone",
+  description: "Tepuk seseorang",
   run: async (client, message, args) => {
     
     let target = message.mentions.members.first()
