85e584abb72ae203d9a2cf8a19c47bba2d7e5a29
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-09 12:56:51 +0700

    Update resetwarn.js

--- commands/moderation/resetwarn.js
+++ commands/moderation/resetwarn.js
@@ -5,37 +5,37 @@ module.exports = {
   aliases: ["rwarns", "rsetwarns"],
   category: "moderation",
   usage: "rwarns <@user>",
-  description: "Reset warnings of mentioned person",
+  description: "Atur ulang peringatan dari orang yang disebutkan",
   run: async (client, message, args) => {
     if (!message.member.hasPermission("ADMINISTRATOR")) {
       return message.channel.send(
-        "Yopu should have admin perms to use this command"
+        "You harus memiliki izin admin untuk menggunakan perintah ini"
       );
     }
 
     const user = message.mentions.members.first();
 
     if (!user) {
-      return message.channel.send("Please mention the person whose warning you want to reset");
+      return message.channel.send("Sebutkan orang yang peringatannya ingin Anda setel ulang");
     }
 
     if (message.mentions.users.first().bot) {
-      return message.channel.send("Bot are not allowed to have warnings");
+      return message.channel.send("Bot tidak boleh memiliki peringatan!");
     }
 
     if (message.author.id === user.id) {
-      return message.channel.send("You are not allowed to reset your warnings");
+      return message.channel.send("Anda tidak diizinkan untuk mengatur ulang peringatan Anda");
     }
 
     let warnings = db.get(`warnings_${message.guild.id}_${user.id}`);
 
     if (warnings === null) {
-      return message.channel.send(`${message.mentions.users.first().username} do not have any warnings`);
+      return message.channel.send(`${message.mentions.users.first().username} tidak memiliki peringatan apapun`);
     }
 
     db.delete(`warnings_${message.guild.id}_${user.id}`);
     user.send(
-      `Your all warnings are reseted by ${message.author.username} from ${message.guild.name}`
+      `Semua peringatan Anda disetel ulang oleh ${message.author.username} from ${message.guild.name}`
     );
     await message.channel.send(
       `Reseted all warnings of ${message.mentions.users.first().username}`
