6da6427957a1b7b8984ee2cf6f3623b92f3753e7
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-09 12:56:20 +0700

    Update mute.js

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -7,24 +7,24 @@ module.exports = {
   usage: "mute <@mention> .<reason>",
   run: async (client, message, args) => {
     if (!message.member.hasPermission("MANAGE_ROLES")) {
-      return message.channel.send("sorry you need permission to mute someone");
+      return message.channel.send("maaf, Anda memerlukan izin untuk ngemute seseorang");
     }
     if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
-      return message.channel.send("I do not have permission to mute");
+      return message.channel.send("Saya tidak memiliki izin untuk ngemute");
     }
 
     const user = message.mentions.members.first();
 
     if (!user) {
-      return message.channel.send("\```please mention the members for mute\```");
+      return message.channel.send("\```tolong sebutkan anggota untuk bisu\```");
     }
     if (user.id === message.author.id) {
-      return message.channel.send("I can't mute you because you are message author");
+      return message.channel.send("Saya tidak dapat menonaktifkan Anda karena Anda adalah penulis pesan");
     }
     let reason = args.slice(1).join("");
 
     if (!reason) {
-      return message.channel.send(" \``` please give some  reason for mute\``` ");
+      return message.channel.send(" \``` tolong beri beberapa alasan untuk ngemute\``` ");
     }
 
     const vrole = user.roles.cache
@@ -32,17 +32,17 @@ module.exports = {
     let muterole = message.guild.roles.cache.find(x => x.name === "muted");
 
     if (!muterole) {
-      return message.channel.send("\```please create role name with muted \``` ");
+      return message.channel.send("\```tolong buat nama role dengan @🌾 Berbunga   \``` ");
     }
     
     await user.roles.remove(vrole);
     await user.roles.add(muterole);
 
     await message.channel.send(
-      `you muted ${message.mentions.users.first().username} for ${reason}`
+      `Kamu muted ${message.mentions.users.first().username} for ${reason}`
     );
 
-    user.send(`You get muted in ${message.guild} for ${reason}`
+    user.send(`Kamu di mute ${message.guild} for ${reason}`
     );
   }
 };
