4bbb8ad2231a9ccda72df3ea665167a35bc72791
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-09 12:47:13 +0700

    Update bug.js

--- commands/help/bug.js
+++ commands/help/bug.js
@@ -3,10 +3,10 @@ module.exports = {
 category: "info",
 run : async(client, message, args) => { 
 // again make this fit your command handler style 😀
-  if (!args[0]) return message.reply("Please specify the bug. Example:\n`/punch isn't working. It isn't mentioning the user I'm trying to punch`");   
-  if (args[0] === "bug") return message.reply("Please specify the bug. Example:\n`/punch isn't working. It isn't mentioning the user I'm trying to punch`");   
+  if (!args[0]) return message.reply("Harap sebutkan bugnya. Contoh: \ n` / punch tidak berfungsi. Itu tidak menyebutkan pengguna yang saya coba pukul`");   
+  if (args[0] === "bug") return message.reply("Harap sebutkan bugnya. Contoh: \ n` / punch tidak berfungsi. Itu tidak menyebutkan pengguna yang saya coba pukul`");   
   args = args.join(" ");   
-  message.reply("Thanks for submitting a bug!");  
+  message.reply("Terima kasih telah mengirimkan bug!");  
   const content = `\`\`\`**${message.author.username}#${message.author.discriminator}** (${message.author.id}) reported:\n~~--------------------------------~~\n${args}\n~~--------------------------------~~\nOn the server: **${message.guild.name}**\nServer ID: **${message.guild.id}**\`\`\``;   
   client.channels.cache.get('812592666286096404').send(content)
 }
