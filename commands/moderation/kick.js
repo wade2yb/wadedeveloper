53e0b688c0575c5b5a10d794ad596525c6fa0861
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-09 12:55:49 +0700

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -4,18 +4,18 @@ const moment = require('moment')
 module.exports = {
 name: "kick",
 category: "moderation",
-description: "kick a user",
+description: "Menendang pengguna",
 cooldown: 5,
 userPerms: ["KICK_MEMBERS"],
 clientPerms: ["KICK_MEMBERS"],
 run: async(client, message, args) => {  
 const mentionedMember = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 const reason = args.slice(1).join(" ")
-          if (!args[0]) return message.channel.send(":x: | **Specify someone to kick.**")
-        if (!mentionedMember) return message.channel.send(":x: | **I can't find that member.**")
-        if (mentionedMember.id === message.author.id) return message.channel.send(":x: | You can't kick yourself.")
+          if (!args[0]) return message.channel.send(":x: | **Tentukan seseorang untuk ditendang.**")
+        if (!mentionedMember) return message.channel.send(":x: | **Saya tidak dapat menemukan anggota itu.**")
+        if (mentionedMember.id === message.author.id) return message.channel.send(":x: | kamu tidak bisa menendang dirimu sendiri.")
         if (mentionedMember.roles.highest.position >= message.member.roles.highest.position && message.author.id !== message.guild.owner.id) {
-            return message.channel.send(":x: | **You can\'t kick this member due to your role being lower than that member role.**")
+            return message.channel.send(":x: | **Anda tidak dapat mengeluarkan anggota ini karena role Anda lebih rendah daripada peran anggota tersebut.**")
         }
         if (mentionedMember.kickable) {
             const embed = new MessageEmbed()
@@ -29,7 +29,7 @@ const reason = args.slice(1).join(" ")
         message.channel.send(embed)
         mentionedMember.kick()
         } else {
-            return message.channel.send(":x: | **I can\'t kick this user make sure that the users role is lower than my role.**")
+            return message.channel.send(":x: | **Saya tidak dapat menendang pengguna ini untuk memastikan bahwa peran pengguna lebih rendah dari peran saya.**")
         }
         return undefined
     }
