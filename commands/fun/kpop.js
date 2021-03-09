360cbb3606d4df81775b8b6dac40b21eaacac849
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-09 12:38:40 +0700

    Update kpop.js

--- commands/fun/kpop.js
+++ commands/fun/kpop.js
@@ -4,7 +4,7 @@ const random = new Random();
 module.exports = {
   name: "kpop",
   category: "fun",
-  description: "Get some kpop singers images with names",
+  description: "Dapatkan beberapa gambar penyanyi kpop dengan nama",
   run: async (client, message, args) => {
     
     let data = await random.getKpop()
