b4f3781865be7091ae95141e2cbfb2b67da2acae
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-09 12:59:08 +0700

    Update welcome.js

--- commands/moderation/welcome.js
+++ commands/moderation/welcome.js
@@ -5,19 +5,19 @@ module.exports = {
   name: "setwelcome",
   category: "moderation",
   usage: "setwelcome <#channel>",
-  description: "Set the welcome channel",
+  description: "Setel saluran selamat datang!",
   run: (client, message, args) => {
     
     let channel = message.mentions.channels.first()
     
     if(!channel) {
-      return message.channel.send("Please Mention the channel first")
+      return message.channel.send("Harap Sebutkan channelnya terlebih dahulu")
     }
     
     //Now we gonna use quick.db
     
     db.set(`welchannel_${message.guild.id}`, channel.id)
     
-    message.channel.send(`Welcome Channel is seted as ${channel}`)
+    message.channel.send(`Saluran Selamat Datang disetel Di ${channel}`)
   }
 }
