a079cb907afd5a7e7cd894f9208c09a0b366876c
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-09 12:42:49 +0700

    Update end.js

--- commands/giveaway/end.js
+++ commands/giveaway/end.js
@@ -1,23 +1,4 @@
-const { MessageEmbed } = require('discord.js')
-const ms = require('ms');
-module.exports = {
-        name: "end",
-        description: "Ending giveaway",
-        accessableby: "Administrator",
-        category: "giveaway",
-        aliases: ["giveaway-end"],
-        usage: '<giveawaymessageid>',
-    run: async (bot, message, args) => {
-      if(!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")){
-        return message.channel.send(':x: You need to have the manage messages permissions to reroll giveaways.');
-    }
-
-    // If no message ID or giveaway name is specified
-    if(!args[0]){
-        return message.channel.send(':x: You have to specify a valid message ID!');
-    }
-
-    // try to found the giveaway with prize then with ID
+// try to found the giveaway with prize then with ID
     let giveaway = 
     // Search with giveaway prize
     bot.giveawaysManager.giveaways.find((g) => g.prize === args.join(' ')) ||
@@ -26,7 +7,7 @@ module.exports = {
 
     // If no giveaway was found
     if(!giveaway){
-        return message.channel.send('Unable to find a giveaway for `'+ args.join(' ') + '`.');
+        return message.channel.send('Tidak dapat menemukan hadiah untuk `'+ args.join(' ') + '`.');
     }
 
     // Edit the giveaway
@@ -36,11 +17,11 @@ module.exports = {
     // Success message
     .then(() => {
         // Success message
-        message.channel.send('Giveaway will end in less than '+(bot.giveawaysManager.options.updateCountdownEvery/1000)+' seconds...');
+        message.channel.send('Giveaway akan berakhir kurang dari '+(bot.giveawaysManager.options.updateCountdownEvery/1000)+' seconds...');
     })
     .catch((e) => {
-        if(e.startsWith(`Giveaway with message ID ${giveaway.messageID} is already ended.`)){
-            message.channel.send('This giveaway is already ended!');
+        if(e.startsWith(`Giveaway dengan ID pesan ${giveaway.messageID} sudah berakhir.`)){
+            message.channel.send('Giveaway ini sudah berakhir!');
         } else {
             console.error(e);
             message.channel.send('An error occured...');
