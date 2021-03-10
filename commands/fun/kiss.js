c6b454d345556f8ede660631693730e8e139eedd
wadee2yb <77784206+wadee2yb@users.noreply.github.com>
2021-03-11 04:02:51 +0700

    Update kiss.js

--- commands/fun/kiss.js
+++ commands/fun/kiss.js
@@ -1,23 +1,40 @@
-const discord = require("discord.js");
-const { Random } = require("something-random-on-discord");
-const random = new Random();
+const Discord = require("discord.js");
+const superagent = require("snekfetch");
 
-module.exports = {
-  name: "kiss",
-  category: "fun",
-  description: "Mencium seseorang",
-  run: async (client, message, args) => {
-    
-    let target = message.mentions.members.first()
-    
-    let data = await random.getAnimeImgURL("kiss");
-    
-    let embed = new discord.MessageEmbed()
-    .setImage(data)
-    .setColor("RANDOM")
-    .setFooter(`${message.author.username} kisses ${target.user.username}`)
-    .setTimestamp()
-    
-    message.channel.send(embed);
-  }
-};
+        module.exports.run = async (client, message, args) => {
+            const user = message.mentions.users.first();
+            if(!user)
+                return message.channel.send({embed: {
+                color: 16734039,
+                description: "You must mention someone to kiss!"
+            }})
+
+		if (message.author === user) {
+           return await message.channel.send({embed: {
+                color: 16734039,
+                description: "You cant kiss yourself!"
+            }})
+		}
+            superagent.get('https://nekos.life/api/v2/img/kiss')
+                .end((err, response) => {
+              const embed = new Discord.RichEmbed()
+              .setTitle(user.username + " Just got a kiss from " + message.author.username)
+              .setImage(response.body.url)
+              .setColor("RANDOM")
+              .setDescription((user.toString() + " got a kiss from " + message.author.toString()))
+              .setFooter(`this is so cute`)
+              .setURL(response.body.url);
+          message.channel.send(embed);
+            }).catch((err) => message.channel.send({embed: {
+                color: 16734039,
+                description: "Something went wrong... :cry:"
+            }}));
+
+        }
+
+module.exports.help = {
+    name: "kiss",
+    description: "Kiss a mentioned user",
+    usage: "kiss <user>",
+    type: "Fun" 
+}
